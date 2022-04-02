e5a1941dbcc1a5bac0bf01db4c91c2ec505cef3e
Barış SAVAŞ <barissavas17@gmail.com>
2022-04-02 17:15:57 +0300

    lifting state ex

---
+++ src/liftState/childrensAsFunctions.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+const App = () => (
+  <div>
+    <h1>US Dollar to Euro:</h1>
+    <Amount>{(amount) => <Euro amount={amount} />}</Amount>
+
+    <h1>US Dollar to Pound:</h1>
+    <Amount>{(amount) => <Pound amount={amount} />}</Amount>
+  </div>
+);
+
+const Amount = ({ children }) => {
+  const [amount, setAmount] = React.useState(0);
+
+  const handleIncrement = () => setAmount(amount + 1);
+  const handleDecrement = () => setAmount(amount - 1);
+
+  return (
+    <div>
+      <button type="button" onClick={handleIncrement}>
+        +
+      </button>
+      <button type="button" onClick={handleDecrement}>
+        -
+      </button>
+
+      <p>US Dollar: {amount}</p>
+      {children(amount)}
+    </div>
+  );
+};
+
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
+
+export default App;
