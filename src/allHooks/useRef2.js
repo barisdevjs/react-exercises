ef0089315ae3b021e0b3e638e0041e7c8a7ac81a
Barış SAVAŞ <barissavas17@gmail.com>
2023-01-30 13:35:36 +0300

    Update useRef2.js

--- src/allHooks/useRef2.js
+++ src/allHooks/useRef2.js
@@ -1,13 +1,18 @@
-import { useRef, useState,useEffect } from 'react';
-import * as React from 'react'
+import { useRef, useState, useEffect } from 'react';
+import * as React from 'react';
+import './style.css';
 
 function App() {
-  const timerIdRef = useRef(0);
-  const [count, setCount] = useState(0);
-  const [targetCount, setTargetCount] = useState(0);
+  const timerIdRef = useRef<number>(0);
+  const [count, setCount] = useState<number>(0);
+  const [targetCount, setTargetCount] = useState<number>(0);
 
   useEffect(() => {
     if (count === targetCount) {
+      stopHandler();
+    }
+
+    if (count === 0) {
       stopHandler();
     }
   }, [count, targetCount]);
@@ -16,8 +21,9 @@ function App() {
     if (timerIdRef.current) {
       return;
     }
-    timerIdRef.current = setInterval(() => setCount(c => c + 1), 100);
+    timerIdRef.current = setInterval(() => setCount((c) => c + 1), 100);
   };
+
   const stopHandler = () => {
     clearInterval(timerIdRef.current);
     timerIdRef.current = 0;
@@ -26,23 +32,29 @@ function App() {
   const resetHandler = () => {
     clearInterval(timerIdRef.current);
     timerIdRef.current = 0;
-    setCount(c => c = 0)
+    setCount((c) => (c = 0));
   };
 
-  const countFive = (number) => {
+  const countToNum = (number: number) => {
     setTargetCount(number);
     startHandler();
-  }
+  };
+
+  const countFromNumber = (number: number) => {
+    setCount(number);
+    setTargetCount(0);
+    timerIdRef.current = setInterval(() => setCount((c) => c - 1), 100);
+  };
 
   return (
-    <div>
-      <div>Timer: {count} s</div>
-      <div>
+    <div className="outer">
+      <h3 className="innerOne">Timer: {count} s</h3>
+      <div className="innerTwo">
         <button onClick={startHandler}>Start</button>
         <button onClick={stopHandler}>Stop</button>
         <button onClick={resetHandler}>Reset</button>
-        <button onClick={() => countFive(5)}>5</button>
-
+        <button onClick={() => countToNum(5)}>5</button>
+        <button onClick={() => countFromNumber(10)}>10</button>
       </div>
     </div>
   );
