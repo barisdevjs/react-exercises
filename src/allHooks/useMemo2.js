4a28bbd4f6426fb87db1ddc190b1eb5ae4881a6c
Barış SAVAŞ <barissavas17@gmail.com>
2023-02-20 00:05:59 +0300

    example added

---
+++ src/allHooks/useMemo2.js
@@ -0,0 +1,16 @@
+import React, { useState, useMemo} from 'react';
+
+function App() {
+  const [numbers] = useState([10,20,30]);
+
+  const total = useMemo(() => numbers.reduce((acc, val) => acc + val, 0), [numbers]);
+    
+
+  return (
+    <div>
+        Total : {total}
+    </div>
+  )
+}
+
+export default App 
