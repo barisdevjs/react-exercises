7ea80b19646b49e5b21d5d92388786eed9f24261
Barış SAVAŞ <barissavas17@gmail.com>
2023-07-16 15:57:08 +0300

    added Ref4

--- src/allHooks/useRef4.js
+++ src/allHooks/useRef4.js
@@ -1,26 +1,83 @@
+/* import React, { useEffect, useRef, useState } from "react";
+
+function* idGenerator() {
+  let id = 3;
+  while (true) {
+    yield id++;
+  }
+}
+
+const idGen = idGenerator();
+
+export default function App() {
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const inputRef = useRef(null);
+  const [names, setNames] = useState([
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+  ]);
+
+  const onAddName = () => {
+    setNames([
+      ...names,
+      { id: idGen.next().value, name: inputRef.current.value },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  return (
+    <>
+      <input type="text" placeholder="Search" ref={inputRef} />
+      <button onClick={onAddName}>Add name</button>
+      <div>
+        {names.map((name) => (
+          <p key={name.id}>
+            {name.id} - {name.name}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+}
+ */
+
 import React, { useEffect, useRef, useState } from "react";
 
 export default function App() {
-  const [name, setName] = useState('');
-  const prevInfo = useRef('')
 
   useEffect(() => {
-    prevInfo.current = name;
-  }, [name])
+    inputRef.current.focus();
+  }, [])
 
-  console.log('name', name, 'prevInfo', prevInfo.current)
+  const [names, setNames] = useState([{id: 1, name: 'John'},{id: 2, name: 'Jane'}]);
+  const idRef = useRef(2);
+  const inputRef = useRef(null);
+
+  const onAddName = () => {
+    const newId = idRef.current + 1;
+    setNames([...names, {id: newId, name: inputRef.current.value}]);
+    idRef.current = newId;
+    inputRef.current.value = '';
+  }
+
+  console.log(idRef.current)
 
   return (
     <>
       <input
         type="text"
         placeholder="Search"
-        value={name}
-        onChange={e => setName(e.target.value)} />
-      <p>My name is {name.toLocaleUpperCase()}
-        and my previous info is
-        {prevInfo.current.toLocaleLowerCase()}
-      </p>
+        ref={inputRef}
+      />
+      <button onClick={onAddName}>Add name</button>
+      <div>
+        {names.map((name) => (
+          <p key={name.id}>{name.id} - {name.name}</p>
+        ))}
+      </div>
     </>
   )
 }
