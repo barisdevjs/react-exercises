c73b2d8c74befd083e6699b73edbb590b2818ff5
Barış SAVAŞ <barissavas17@gmail.com>
2024-01-14 21:48:02 +0300

    App.js changed

--- src/App.js
+++ src/App.js
@@ -1,78 +1,25 @@
-import { useReducer } from 'react';
+import React, { useRef, useState } from "react";
 
-const formReducer = (state, action) => ({ ...state, ...action });
+export default function App() {
+  const [name, setName] = useState('');
+  const inputRef = useRef()
+  console.log(name) // it will not trigger the useState
 
-function UserForm() {
-  const [state, dispatch] = useReducer(formReducer, {
-    first: 'F',
-    last: 'L'
-  });
+  function focus() {
+    inputRef.current.focus();
+    inputRef.current.value = 'manual entering';
+  }
 
-    return (
-        <div>
-            <h1>User Form</h1>
-            <input type="text" value={state.first}
-                onChange={(e) => dispatch({ first: e.target.value })} />
-            <input type="text" value={state.last}
-                onChange={(e) => dispatch({ last: e.target.value })} />
-            <div>First Name: {state.first}</div>
-            <div>Last Name: {state.last}</div>
-        </div>
-    )
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search"
+        value={name}
+        ref={inputRef}
+        onChange={e => setName(e.target.value)} />
+      <p>My name is {name}</p>
+      <button onClick={focus}>Focus</button>
+    </>
+  )
 }
-
-// Action types
-const SET_NAME = 'SET_NAME';
-const ADD_NAME = 'ADD_NAME';
-
-// Action creators
-const setName = (payload) => ({ type: SET_NAME, payload });
-const addName = () => ({ type: ADD_NAME });
-
-// Reducer
-const reducer = (state, action) => {
-    switch (action.type) {
-        case SET_NAME:
-            return { ...state, name: action.payload };
-        case ADD_NAME:
-            return { ...state, names: [...state.names, state.name], name: '' };
-        default:
-            return state;
-    }
-};
-
-function NameList() {
-    const [state, dispatch] = useReducer(reducer, { names: [], name: 'Default State' });
-
-    return (
-        <div className="App">
-            <h1>Name List</h1>
-            <input
-                type="text"
-                value={state.name}
-                onChange={(event) => dispatch(setName(event.target.value))}
-            />
-            <p>{state.name}</p>
-            <button onClick={() => dispatch(addName())}>ADD NAME</button>
-            <hr />
-            <div>
-                {state.names.map((name, index) => (
-                    <p key={index}>{name}</p>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-
-function App() {
-    return (
-        <>
-            <UserForm />
-            <hr></hr>
-            <NameList />
-        </>
-    )
-}
-
-export default App
